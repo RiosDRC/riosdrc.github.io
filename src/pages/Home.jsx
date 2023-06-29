91df9c9367851be5702514b192f22e82bd35722b
riosdrc <riosdrc@gmail.com>
2023-06-28 20:38:46 -0600

    Convert to React

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to my Github page</h1>
+            <div className="projects">
+                <div className="project">
+                    <h2>Shoes</h2>
+                    <Link to="/shoes">
+                        <img src="./img/Shoes.jpg" alt="" />
+                        <button>Visit</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
