fc42f6b510f81dd1413542dd59ce5cc5a4713ff7
riosdrc <riosdrc@gmail.com>
2023-07-01 12:13:59 -0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
             <div className="projects">
                 <div className="project">
                     <h2>Shoes</h2>
-                    <Link to="https://riosdrc.github.io/shoes">
+                    <Link to="https://riosdrc.github.io/shoes/">
                         <img src="./img/Shoes.jpg" alt="" />
                         <button>Visit</button>
                     </Link>
                 </div>
                 <div className="project">
                     <h2>GTP3</h2>
-                    <Link to="https://riosdrc.github.io/gpt3">
+                    <Link to="https://riosdrc.github.io/gpt3/">
                         <img src="./img/gpt3.jpg" alt="" />
                         <button>Visit</button>
                     </Link>
