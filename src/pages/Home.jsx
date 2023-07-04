f96b77a1de60f0fde3add6f6b7ccf392531d5c2f
riosdrc <riosdrc@gmail.com>
2023-07-04 14:49:43 -0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,28 @@ const Home = () => {
                 <div className="project">
                     <h2>Shoes</h2>
                     <a href="https://riosdrc.github.io/shoes/">
-                        <img src="./img/Shoes.jpg" alt="" />
+                        <img src="./img/shoes.jpg" alt="shoes" />
                         <button>Visit</button>
                     </a>
                 </div>
                 <div className="project">
                     <h2>GTP3</h2>
                     <a href="https://riosdrc.github.io/gpt3/">
-                        <img src="./img/gpt3.jpg" alt="" />
+                        <img src="./img/gpt3.jpg" alt="gtp3" />
                         <button>Visit</button>
                     </a>
                 </div>
                 <div className="project">
                     <h2>GTP3</h2>
                     <a href="https://riosdrc.github.io/restaurant/">
-                        <img src="./img/restaurant.jpg" alt="" />
+                        <img src="./img/restaurant.jpg" alt="restaurant" />
+                        <button>Visit</button>
+                    </a>
+                </div>
+                <div className="project">
+                    <h2>Bank Modern App</h2>
+                    <a href="https://riosdrc.github.io/bank_modern_app/">
+                        <img src="./img/bank.jpg" alt="bank" />
                         <button>Visit</button>
                     </a>
                 </div>
