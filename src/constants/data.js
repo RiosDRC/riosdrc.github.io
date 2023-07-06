2079fdd08b7e2aea0bc7ca1c8d399a3cf65aa27a
riosdrc <riosdrc@gmail.com>
2023-07-06 11:11:47 -0600

    reset

---
+++ src/constants/data.js
@@ -0,0 +1,59 @@
+const projects = [
+    {
+        id: 1,
+        title: "Shoes Store",
+        img: "./img/shoes.jpg",
+        url: "https://riosdrc.github.io/shoes/",
+        desc: ""
+    },
+    {
+        id: 2,
+        title: "GPT-3",
+        img: "./img/gpt3.jpg",
+        url: "https://riosdrc.github.io/gpt3/",
+        desc: ""
+    },
+    {
+        id: 3,
+        title: "Restaurant",
+        img: "./img/restaurant.jpg",
+        url: "https://riosdrc.github.io/restaurant/",
+        desc: ""
+    },
+    {
+        id: 4,
+        title: "Bank Modern App",
+        img: "./img/bank.jpg",
+        url: "https://riosdrc.github.io/bank_modern_app/",
+        desc: ""
+    }
+];
+
+const education = [
+    {
+        id: 1,
+        inst: "University of Costa Rica",
+        area: "Barchelor's Degree in English Teaching",
+        year: "2013 - 2021"
+    },
+    {
+        id: 2,
+        inst: "Harvard Online Courses",
+        area: "CS50 - Introduction to Computer Science, Python, and Web Design",
+        year: "2022 - 2023"
+    },
+    {
+        id: 3,
+        inst: "Alisson Courses",
+        area: "Introduction to React.js",
+        year: "2023"
+    },
+    {
+        id: 4,
+        inst: "The Net Ninja, Solo Python, and Lama Dev",
+        area: "Web tutorials: React, Django Restframework, Node.js",
+        year: "2023"
+    }
+];
+
+export {education, projects};
