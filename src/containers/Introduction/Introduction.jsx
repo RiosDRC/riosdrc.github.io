4bc5c0b94289c1b19205ba70aec65b23bd192e5b
riosdrc <riosdrc@gmail.com>
2023-09-11 14:31:18 -0600

    Remove github sponsor

--- src/containers/Introduction/Introduction.jsx
+++ src/containers/Introduction/Introduction.jsx
@@ -4,15 +4,15 @@ import "./Introduction.css"
 const Introduction = () => {
   return (
     <div className="app__intro" id='home'>
-        <h1>Welcome to my Github WebPage</h1>
+        <h1>Welcome to my WebPage</h1>
 
-        <div className="app__intro-content">
+        {/* <div className="app__intro-content">
             <p>This webpage was designed as a landing page to host front-end projects,
             utilizing the functionality of Github to deploy websites. It serves as a
             showcase for my portfolio, displaying my skills and expertise in front-end development.
             Additionally, the webpage includes personal information about me, the designer, to provide
             potential employers with an insight into my background and qualifications.</p>
-        </div>
+        </div> */}
     </div>
   )
 }
